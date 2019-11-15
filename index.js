d058a65b54f286dd9375f082f8d044a63780139f
Fleuron Gnadekpa <vilfleur@gmail.com>
2019-11-15 03:43:41 +0100

    Make Pomodoro stateful

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-function PomodoroClock() {
-  return (
-    <React.Fragment>
-      <h1>Pomodoro Clock</h1>
-      <Setting label="session" />
-      <Setting label="break" />
-      <Controls />
-    </React.Fragment>
-  )
+class PomodoroClock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      session: 25,
+      pause: 5
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Pomodoro Clock</h1>
+        <Setting label="session" length={this.state.session}/>
+        <Setting label="break" length={this.state.pause}/>
+        <Controls />
+      </React.Fragment>
+    )
+  }
 }
 
 ReactDOM.render(
